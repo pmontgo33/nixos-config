1c88b92ee81fc1469fb0f0f9bd79242ee47ab5d6
Monty <21371673+pmontgo33@users.noreply.github.com>
2024-08-10 21:40:12 -0400

    fixed comment typo

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -2,6 +2,12 @@
 For installation on LXC, add these lines to /etc/pve/lxc/ID.conf:
 lxc.cgroup2.devices.allow: c 10:200 rwm
 lxc.mount.entry: /dev/net/tun dev/net/tun none bind,create=file
+
+For LXC, run:
+tailscale up --ssh --accept-dns=false
+For other, run:
+tailscale up --ssh
+
 */
 
 {
