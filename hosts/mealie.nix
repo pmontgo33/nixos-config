9886ada50c55cae75153298c23bb26db6c2e771e
Monty <21371673+pmontgo33@users.noreply.github.com>
2024-12-10 22:14:15 -0500

    changed mealie to unstable

--- hosts/mealie.nix
+++ hosts/mealie.nix
@@ -1,12 +1,17 @@
-{ pkgs, ... }: 
+{ config, pkgs, ... }:
 
+let
+  unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {
+    config = config.nixpkgs.config;
+  };
+in
 {
   imports = [
     ../base/base_config.nix
   ];
 	
   environment.systemPackages = with pkgs; [
-		mealie
+		unstable.mealie
   ];
   
   services.mealie.enable = true;
